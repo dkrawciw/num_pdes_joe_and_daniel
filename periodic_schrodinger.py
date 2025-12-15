56640262fa88c9fc88c85049375ff9663786341f
dkrawciw <daniel.krawciw@gmail.com>
2025-12-15 15:19:15 -0700

    Fixed math for periodic_SE

--- periodic_schrodinger.py
+++ periodic_schrodinger.py
@@ -1,6 +1,6 @@
 import numpy as np
-from scipy.sparse import csr_matrix, kron, eye
-from scipy.sparse.linalg import spsolve
+from scipy.sparse import csr_matrix, kron, eye, csc_matrix
+from scipy.sparse.linalg import spsolve, splu
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -31,11 +31,6 @@ y_vals = np.linspace(y_range[0], y_range[1], N,endpoint=False)
 X,Y = np.meshgrid(x_vals,y_vals, indexing='ij')
 h = x_vals[1] - x_vals[0]
 
-"""Time Setup"""
-N_t = 100
-t_range = [0, 10]
-dt = (t_range[1] - t_range[0]) / N_t
-
 """Construct the periodic Laplacian"""
 D = np.zeros((N,N))
 D += np.eye(N, k=0) * -2
@@ -43,35 +38,45 @@ D += np.eye(N, k=1)
 D += np.eye(N, k=-1)
 D[-1,0] = 1
 D[0,-1] = 1
+D /= h**2
 
-D = csr_matrix(D)
+D = csc_matrix(D)
 
-L = kron(eye(N, format="csr"), D, format="csr") + kron(D, eye(N, format="csr"), format="csr")
+L = kron(eye(N, format="csc"), D, format="csc") + kron(D, eye(N, format="csc"), format="csc")
 
-nonlinear_func = lambda u: u * np.linalg.norm(u)**2
+"""Time Setup"""
+N_t = 1000
+t_range = [0, 1]
+dt = (t_range[1] - t_range[0]) / N_t
 
 """Creating the solution array"""
 U = np.zeros((N,N,N_t), dtype=complex)
 U[0,N//2,0] = 1
 
+"""Begin evolving the solution in time"""
+nonlinear_func = lambda u: u * np.abs(u)**2
+
+LHS = eye(N**2, format="csc") + 1j*dt/2 * L
+LHS_lu = splu(LHS)
+
 # Run forward 1 time step
 u_now = U[:,:,0].ravel(order="F")
-LHS = eye(N**2, format="csr") - dt/2 * L
-RHS = (eye(N**2, format="csr") + dt/2 * L)@u_now + dt*nonlinear_func(u_now)
-u_then = spsolve(LHS,RHS)
+RHS = (eye(N**2, format="csc") - 1j*dt/2 * L)@u_now - 1j*dt*nonlinear_func(u_now) - 1j*dt
+u_then = LHS_lu.solve(RHS)
 U[:,:,1] = u_then.reshape((N,N), order="F")
 
-for k in tqdm(range(1,N_t-1), desc="Evolving SE"):
+"""Actually move it forward with CN-AB2"""
+for k in tqdm(range(1,N_t-1), desc="Evolving SE", colour="green"):
     u_now = U[:,:,k].ravel(order="F")
     u_prev = U[:,:,k-1].ravel(order="F")
 
-    LHS = eye(N**2, format="csr") - 1j*dt/2 * L
-    RHS = (eye(N**2, format="csr") + 1j*dt/2 * L)@u_now + 1j*3/2 * dt * nonlinear_func(u_now) - 1j*dt/2 * nonlinear_func(u_prev)
+    RHS = (eye(N**2, format="csc") - 1j*dt/2 * L)@u_now - 1j*3/2 * dt * nonlinear_func(u_now) + 1j*dt/2 * nonlinear_func(u_prev)
 
-    u_then = spsolve(LHS,RHS)
+    # u_then = spsolve(LHS,RHS)
+    u_then = LHS_lu.solve(RHS)
+    # u_then = u_then / np.linalg.norm(u_then)
 
     U[:,:,k+1] = u_then.reshape((N,N), order="F")
-
 
 """Plotting the first and last solution """
 fig, ax = plt.subplots(1, 2, figsize=(12, 5))
@@ -96,4 +101,4 @@ plt.suptitle('Evolution of Schrodinger Equation Through Time', fontsize=18)
 
 plt.tight_layout()
 # plt.savefig("output/problem3.png", dpi=300)
-plt.savefig("output/problem3.svg")
+plt.savefig("output/initial_and_final_SE.svg")
